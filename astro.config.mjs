aae767424b02c67a82503fa859b61dd38471690f
Lucas Dunker <dunker.l@northeastern.edu>
2024-02-12 23:37:38 -0500

    add sitemap + rss support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), sitemap()]
 });
