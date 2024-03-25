75a3b3bb1724f252886d7b7bf8857cad334c88e0
Lucas Dunker <dunker.l@northeastern.edu>
2024-03-24 20:29:02 -0400

    Prettier formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
@@ -9,5 +9,5 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [tailwind(), react(), sitemap()]
+  integrations: [tailwind(), react(), sitemap()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,28 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
-	darkMode: 'class',
-	theme: {
-		fontFamily: {
-			sans: ['Inter', ...defaultTheme.fontFamily.sans],
-			serif: ['Newsreader', ...defaultTheme.fontFamily.serif]
-		},
-		extend: {
-			textColor: {
-				main: 'rgb(var(--color-text-main) / <alpha-value>)'
-			},
-			backgroundColor: {
-				main: 'rgb(var(--color-bg-main) / <alpha-value>)',
-				muted: 'rgb(var(--color-bg-muted) / <alpha-value>)'
-			},
-			borderColor: {
-				main: 'rgb(var(--color-border-main) / <alpha-value>)'
-			},
-		}
-	},
-	plugins: [require('@tailwindcss/typography')]
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      serif: ["Newsreader", ...defaultTheme.fontFamily.serif],
+    },
+    extend: {
+      textColor: {
+        main: "rgb(var(--color-text-main) / <alpha-value>)",
+      },
+      backgroundColor: {
+        main: "rgb(var(--color-bg-main) / <alpha-value>)",
+        muted: "rgb(var(--color-bg-muted) / <alpha-value>)",
+      },
+      borderColor: {
+        main: "rgb(var(--color-border-main) / <alpha-value>)",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwindcss-animated"),
+  ],
 };
