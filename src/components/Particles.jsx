20ba33232b0beaeee598dbf8ca7432503006102d
Lucas Dunker <dunker.l@northeastern.edu>
2024-03-31 00:47:51 -0400

    Particles Engine fade-in

--- src/components/Particles.jsx
+++ src/components/Particles.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+import { loadSlim } from "@tsparticles/slim";
 
 export const ParticlesEngine = () => {
   const [init, setInit] = useState(false);
@@ -8,9 +8,6 @@ export const ParticlesEngine = () => {
   // this should be run only once per application lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
       await loadSlim(engine);
     }).then(() => {
       setInit(true);
@@ -91,6 +88,7 @@ export const ParticlesEngine = () => {
         id="tsparticles"
         particlesLoaded={particlesLoaded}
         options={options}
+        className="animate-fade animate-delay-500"
       />
     );
   }
