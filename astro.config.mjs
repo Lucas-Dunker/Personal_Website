ffe905a2a5bb9aa7df3a57101867018fb99b45ab
Lucas Dunker <dunker.l@northeastern.edu>
2024-07-06 23:28:43 -0400

    Redo icons, fix icons not loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,21 @@ import { defineConfig, passthroughImageService } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://lucasdunker.com",
-  integrations: [tailwind(), react(), sitemap()],
-  adapter: cloudflare({
-  }),
+  integrations: [tailwind(), react(), sitemap(), icon()],
+  adapter: cloudflare({}),
   image: {
-    service: passthroughImageService(),
+    service: passthroughImageService()
   },
   prefetch: {
     defaultStrategy: "load",
-    prefetchAll: true,
-  },
+    prefetchAll: true
+  }
 });
